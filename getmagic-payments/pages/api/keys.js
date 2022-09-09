25c8b13fa1b51de3c7a97d3a46b4871809d6b703
andylnkd <dhalwani1@gmail.com>
2022-09-09 23:42:57 +0000

    added Stripes APIs, created GetMagic's own API and tested it in production. Working great!

---
+++ getmagic-payments/pages/api/keys.js
@@ -0,0 +1,12 @@
+export default function handler(req, res){
+
+    if(req.method === 'GET'){
+        res.status(200).json({
+            publishableKey: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+        }
+        )
+    } else{
+        res.status(405).end('Method not allowed')
+    }
+
+}
