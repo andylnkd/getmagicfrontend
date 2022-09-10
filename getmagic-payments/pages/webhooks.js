134a20bb5980f609a7cf2c40640c8db4db97d330
andylnkd <dhalwani1@gmail.com>
2022-09-09 19:15:18 -0700

    added more to handler to intercept stripe payment call and use the signing secret

--- getmagic-payments/pages/webhooks.js
+++ getmagic-payments/pages/webhooks.js
@@ -9,6 +9,18 @@ api:{
 export default async function webhookhandler(req, res) {
   const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY)
   if (req.method === "POST") {
-    console.log(req.body);
+    const buf = await buffer(req)
+    const sig = req.headers[stripe-signature]
+    const webhooksecret = process.env.NEXT_PUBLIC_STRIPE_WEBHOOK_SIGNING_SECRET
+  let event;
+  try{
+ if (!sig || !webhooksecret) return;
+ event = stripe.webhooks.constructEvent(buf, sig, webhooksecret)
+  }catch(err){console.log(`Webhook error: ${err.message}`);
+  return res.status(400).send(`Webhook error: ${err.message}`)}
+  
+  console.log('event: ',event)
+  res.status(200).send();
   }
+  
 }
