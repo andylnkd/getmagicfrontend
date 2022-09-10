6182c7f167145ce32f6dd6582f768312db6c85fd
andylnkd <dhalwani1@gmail.com>
2022-09-09 19:04:26 -0700

    Learnt how to use Buffer, local config and got Stripe CLI working

--- getmagic-payments/pages/webhooks.js
+++ getmagic-payments/pages/webhooks.js
@@ -1,4 +1,13 @@
-export default function webhookhandler(req, res) {
+import Stripe from 'stripe'
+import {buffer} from 'micro'
+
+export const config = {
+api:{
+  bodyParser: false
+}
+}
+export default async function webhookhandler(req, res) {
+  const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY)
   if (req.method === "POST") {
     console.log(req.body);
   }
